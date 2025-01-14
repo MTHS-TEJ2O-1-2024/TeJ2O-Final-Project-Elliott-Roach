6f2c4d831460cf7ccd9db738ffa24ca928cf916b
Elliott-Roach <elliott.roach@mths.ca>
2025-01-14 11:03:08 -0500

    LEDS

--- main.ts
+++ main.ts
@@ -1,5 +1,4 @@
 /* Copyright (c) 2020 MTHS All rights reserved
- *
  * Created by: Elliott Roach
  * Created on: Jan 2025
  * This program controls a remote control car
@@ -11,20 +10,27 @@ radio.setGroup(88)
 
 //sending forward
 input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
     radio.sendNumber(1)
+    basic.showString("F")
 })
 
 //sending left
 input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
     radio.sendNumber(3)
+    basic.showString("L")
 })
 
 //sending right
 input.onButtonPressed(Button.B, function () {
+    basic.clearScreen()
     radio.sendNumber(5)
+    basic.showString("R")
 })
 //sending stop
 input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
     radio.sendNumber(0)
 })
 
@@ -43,7 +49,6 @@ basic.forever(function () {
     if (receivdCommand == 5) {
         robotbit.StpCarTurn(5, 51, 125)
     }
-
 
     //going left
     if (receivdCommand == 3) {
